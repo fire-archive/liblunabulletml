c26c6e81410533147fddca997040de4d6be41ebe
K. S. Ernest (iFIre) Lee <ernest.lee@chibifire.com>
2019-05-02 23:04:05 -0700

    Update for Mac.

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 cd thirdparty/godot && scons platform=osx target=release_debug separate_debug_symbols=true -j6 use_lto=no gdnative_wrapper=yes deprecated=no && cd ../../ && thirdparty/godot/bin/godot.mac.opt.tools.64 --gdnative-generate-json-api thirdparty/godot-cpp/godot_api.json && cd thirdparty/godot-cpp && scons -j6 use_custom_api_file=yes custom_api_file=godot_api.json platform=osx generate_bindings=yes target=release && cd ../../
-cd thirdparty/godot-cpp && scons -j6 platform=osx generate_bindings=yes target=debug && cd ../../
+cd thirdparty/godot-cpp && scons -j6 platform=osx generate_bindings=yes target=release && cd ../../
